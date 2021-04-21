cad37d73433265d702ae74bc7e91b79030f44b72
torbas <t.orbasido@gmail.com>
2021-04-21 10:38:54 -0700

    remove excess css from loginform component

--- frontend/components/LoginForm.vue
+++ frontend/components/LoginForm.vue
@@ -92,47 +92,6 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.card-body {
-  font-family: "granville",Georgia,"Times New Roman",serif;
-}
-
-.title {
-  font-family: 'Quicksand',
-  'Source Sans Pro',
-  -apple-system,
-  BlinkMacSystemFont,
-  'Segoe UI',
-  Roboto,
-  'Helvetica Neue',
-  Arial,
-  sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 
 button.btn.btn-primary[type=submit]{
   background-color: #023B30;
